7f96a877ec9a9e2cee4ccf45e512d7058e7e01bf
Nikita Korzhuk <ssamplerr6@gmail.com>
2024-03-17 18:41:36 +0300

    test-docker-actions

--- src/main.py
+++ src/main.py
@@ -1,6 +1,5 @@
-from api.api import router as api_router
 from fastapi import FastAPI
 
 app = FastAPI(Title="AnalyzeMate-server")
 
-app.include_router(api_router)
+# app.include_router(api_router)
