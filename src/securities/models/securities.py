8a7b84b804d9a4b74a3dd8ee9035b86040d0c092
sampler6 <80578383+sampler6@users.noreply.github.com>
2024-03-21 22:05:51 +0300

    add uid to securities

--- src/securities/models/securities.py
+++ src/securities/models/securities.py
@@ -6,8 +6,9 @@ from sqlalchemy.orm import Mapped, mapped_column
 class Securities(Base):
     __tablename__ = "securities"
 
-    figi: Mapped[str] = mapped_column(String, primary_key=True)
-    ticker: Mapped[str] = mapped_column(String)
+    uid: Mapped[str] = mapped_column(String, primary_key=True)
+    figi: Mapped[str]
+    ticker: Mapped[str]
     name: Mapped[str]
     price_history: Mapped[int] = mapped_column(Integer, ForeignKey("price_history.id"))
     description: Mapped[str] = mapped_column(nullable=True)
