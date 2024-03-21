eac916fc6b050503e4624accbeb18c36b8b582f8
Nikita Korzhuk <ssamplerr6@gmail.com>
2024-03-21 21:39:29 +0300

    fix securities and price_history

--- src/securities/models/price_history.py
+++ src/securities/models/price_history.py
@@ -9,5 +9,5 @@ class PriceHistory(Base):
     __tablename__ = "price_history"
 
     id: Mapped[int] = mapped_column(Integer, autoincrement=True, primary_key=True)
-    Price: Mapped[float]
+    price: Mapped[float]
     timestamp: Mapped[datetime] = mapped_column(DateTime(timezone=True), server_default=func.now())
--- src/securities/models/securities.py
+++ src/securities/models/securities.py
@@ -6,7 +6,8 @@ from sqlalchemy.orm import Mapped, mapped_column
 class Securities(Base):
     __tablename__ = "securities"
 
-    Ticker: Mapped[str] = mapped_column(String, primary_key=True)
+    figi: Mapped[str] = mapped_column(String, primary_key=True)
+    ticker: Mapped[str] = mapped_column(String)
     name: Mapped[str]
     price_history: Mapped[int] = mapped_column(Integer, ForeignKey("price_history.id"))
-    description: Mapped[str]
+    description: Mapped[str] = mapped_column(nullable=True)
