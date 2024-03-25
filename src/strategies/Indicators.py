c101f254368b770a25483807afcc9fba627f58c7
Gashkov Saveliy <120210549+Nemo121007@users.noreply.github.com>
2024-03-26 00:33:22 +0300

    добавлен идентификатор бижи в глобальные переменые. добавленны файлы Services и Graphic с реализацией соответсвующих классов. реализация не окончательна. начата реализация файла и класса Indicators. структура классов и папок будет установлена позднее

---
+++ src/strategies/Indicators.py
@@ -0,0 +1,57 @@
+from datetime import datetime, timedelta
+
+import Graphic
+import Servises
+from tinkoff.invest import CandleInterval
+
+
+class Indicators:
+    @staticmethod
+    def CalculateRSI(data: dict, window: int):  # type: ignore
+        history = data["history"][1:]  # пропускаем строку заголовков
+        time = [entry[0] for entry in history]
+        close_prices = [entry[2] for entry in history]
+        ema_values = []
+        alpha = 2 / (window + 1)  # Коэффициент сглаживания
+
+        # Вычисление начального значения EMA (простое скользящее среднее первых window значений)
+        sma = sum(close_prices[:window]) / window
+        ema_values.append(sma)
+
+        # Словарь для хранения результата
+        result = {
+            "figi": data["figi"],
+            "ticker": data["ticker"],
+            "timeframe": data["timeframe"],
+            "indicator": "EMA",
+            "history": [["time", "value"]],
+        }
+
+        # Вычисление EMA для остальных точек
+        for i in range(window, len(close_prices)):
+            ema = (close_prices[i] - ema_values[-1]) * alpha + ema_values[-1]
+            ema_values.append(ema)
+            result["history"].append([time[i], ema])
+
+        return result
+
+
+if __name__ == "__main__":
+    shares = Servises.Services.GetHistoricCandle(
+        Ticker="SBER",
+        From=datetime.utcnow() - timedelta(days=400),
+        To=datetime.utcnow(),
+        Interval=CandleInterval.CANDLE_INTERVAL_DAY,
+        IntegerRepresentationTime=False,
+    )
+
+    a = Indicators.CalculateRSI(shares, 10)
+    indicator = {
+        "black": {
+            "history": a,
+            "alpha": 1,  # Пример значения прозрачности (от 0 до 1)
+        }
+    }
+
+    Graphic.Graphic.PrintGraphic(shares, indicator)
+    print(a)
---
+++ src/strategies/Servises.py
@@ -0,0 +1,149 @@
+from datetime import datetime, timedelta
+from time import sleep
+from typing import Any
+
+from config import STOCK_MARKET, TOKEN
+from tinkoff.invest import CandleInterval, Client, InstrumentIdType, Share
+
+
+class Services:
+    @staticmethod
+    def GetShares(Ticker: str) -> Share:
+        """
+        Получает информацию о ценной бумаге по её тикеру.
+
+        :param Ticker: Тикер ценной бумаги.
+        :type Ticker: str
+        :return: Информация о ценной бумаге.
+        :rtype: Instrument or Exception
+        """
+        with Client(TOKEN) as client:
+            try:
+                share = client.instruments.share_by(
+                    id_type=InstrumentIdType.INSTRUMENT_ID_TYPE_TICKER, class_code=STOCK_MARKET, id=Ticker
+                )
+                return share.instrument
+            except:  # noqa
+                # SDK выкидывает ошибки в консоль, что слишком геморно отлавливать
+                return Exception("Отсутсвует бумага с указанным  тикером")
+
+    @staticmethod
+    def GetHistoricCandle(
+        Ticker: str, From: datetime, To: datetime, Interval: CandleInterval, IntegerRepresentationTime: bool
+    ) -> dict:
+        """
+        Получает исторические данные свечей для заданного тикера и временного интервала.
+        :param Token: str
+            Ключ доступа (токен) к API Tinkoff Invest.
+        :param Ticker: str
+            Тикер инструмента.
+        :param From: datetime.datetime
+            Начальная дата и время периода.
+        :param To: datetime.datetime
+            Конечная дата и время периода.
+        :param Interval: enum CandleInterval
+            Временной интервал свечей (CandleInterval).
+        :param IntegerRepresentationTime: bool
+            True - Десятичное представление времени(мс)
+            False - Строкое представление времени.
+        :return: dict
+            Словарь с информацией об инструменте.
+
+        Во избежание превышения лимита запросов внутри присутсвует кулдаун
+        """
+        sizeIntervalDays = timedelta(days=0)
+        if (
+            Interval == CandleInterval.CANDLE_INTERVAL_1_MIN
+            or Interval == CandleInterval.CANDLE_INTERVAL_2_MIN
+            or (Interval == CandleInterval.CANDLE_INTERVAL_3_MIN or Interval == CandleInterval.CANDLE_INTERVAL_5_MIN)
+            or (Interval == CandleInterval.CANDLE_INTERVAL_10_MIN or Interval == CandleInterval.CANDLE_INTERVAL_15_MIN)
+        ):
+            sizeIntervalDays = timedelta(days=1)
+        elif Interval == CandleInterval.CANDLE_INTERVAL_30_MIN:
+            sizeIntervalDays = timedelta(days=2)
+        elif Interval == CandleInterval.CANDLE_INTERVAL_HOUR:
+            sizeIntervalDays = timedelta(days=7)
+        elif Interval == CandleInterval.CANDLE_INTERVAL_4_HOUR:
+            sizeIntervalDays = timedelta(days=31)
+        elif Interval == CandleInterval.CANDLE_INTERVAL_DAY:
+            sizeIntervalDays = timedelta(days=366)
+        elif Interval == CandleInterval.CANDLE_INTERVAL_WEEK:
+            sizeIntervalDays = timedelta(days=732)
+        elif Interval == CandleInterval.CANDLE_INTERVAL_MONTH:
+            sizeIntervalDays = timedelta(days=3660)
+
+        with Client(TOKEN) as client:
+            share = Services.GetShares("SBER")
+            dic = {}
+            dic["figi"] = share.figi
+            dic["ticker"] = Ticker
+            dic["timeframe"] = str(Interval).split(".")[1]
+            m = [["time", "open", "close", "high", "low", "volume"]]
+            currentDate = From
+            c = 0
+            while currentDate + sizeIntervalDays < To:
+                r = client.market_data.get_candles(
+                    figi=share.figi, from_=currentDate, to=currentDate + sizeIntervalDays, interval=Interval
+                )
+
+                for i in range(len(r.candles)):
+                    op = Services._cast_money(r.candles[i].open)
+                    close = Services._cast_money(r.candles[i].close)
+                    high = Services._cast_money(r.candles[i].high)
+                    low = Services._cast_money(r.candles[i].low)
+                    volume = r.candles[i].volume
+
+                    if IntegerRepresentationTime:  # noqa
+                        date = r.candles[i].time.timestamp() * 1000
+                    else:
+                        date = r.candles[i].time
+
+                    m.append([date, float(op), float(close), float(high), float(low), float(volume)])  # type: ignore
+
+                currentDate += sizeIntervalDays
+
+                #   Кулдаун в целях обхода лимитов на запросы
+                sleep(0.2)
+
+                #   Счётчик для отображения количества запросов к API
+                #   Возможно удаление без последствий
+                print(c)
+                c += 1
+
+            r = client.market_data.get_candles(figi=share.figi, from_=currentDate, to=To, interval=Interval)
+
+            for i in range(len(r.candles)):
+                op = Services._cast_money(r.candles[i].open)
+                close = Services._cast_money(r.candles[i].close)
+                high = Services._cast_money(r.candles[i].high)
+                low = Services._cast_money(r.candles[i].low)
+                volume = r.candles[i].volume
+
+                if IntegerRepresentationTime:  # noqa
+                    date = r.candles[i].time.timestamp() * 1000
+                else:
+                    date = r.candles[i].time
+
+                m.append([date, float(op), float(close), float(high), float(low), float(volume)])  # type: ignore
+
+            dic["history"] = m
+        return dic
+
+    @staticmethod
+    def _cast_money(v: Any) -> float:
+        return v.units + v.nano / 1e9
+
+
+pass
+
+if __name__ == "__main__":
+    from datetime import datetime, timedelta
+
+    shares = Services.GetHistoricCandle(
+        Ticker="SBER",
+        From=datetime.utcnow() - timedelta(days=1000),
+        To=datetime.utcnow(),
+        Interval=CandleInterval.CANDLE_INTERVAL_DAY,
+        IntegerRepresentationTime=False,
+    )
+    print(shares)
