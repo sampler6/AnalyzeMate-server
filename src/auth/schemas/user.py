ece4f682c33449f9ff55371798973a1b98d1a917
sampler6 <80578383+sampler6@users.noreply.github.com>
2024-03-25 12:58:48 +0300

    Dev (#30)
    * add securities models, create db tables
    
    * fix securities and price_history
    
    * add uid to securities
    
    * Add Redis and Auth
    
    * added redis
    
    * add user
    
    * add auth
    
    * fix user schemas (#29)
    
    * Dev (#28)
    
    * add securities models, create db tables
    
    * fix securities and price_history
    
    * add uid to securities
    
    * Add Redis and Auth
    
    * added redis
    
    * add user
    
    * add auth
    
    * fix user schemas

--- src/auth/schemas/user.py
+++ src/auth/schemas/user.py
@@ -2,30 +2,31 @@ from datetime import datetime
 from typing import Optional
 
 from fastapi_users import schemas
+from pydantic import Field
 
 
 class UserRead(schemas.BaseUser[int]):
     balance: float
+    patronymic: str
+    name: str
+    surname: str
+    birthdate: datetime
+    options: Optional[str]
+
+
+class UserCreate(schemas.BaseUserCreate):
+    balance: Optional[float]
+    patronymic: str
+    name: str
+    surname: str
+    birthdate: datetime = Field(default=datetime.utcnow())
+    options: Optional[str]
+
+
+class UserUpdate(schemas.BaseUserUpdate):
+    balance: Optional[float]
     patronymic: Optional[str]
     name: Optional[str]
     surname: Optional[str]
     birthdate: Optional[datetime]
     options: Optional[str]
-
-
-class UserCreate(schemas.BaseUserCreate):
-    balance: Optional[float] = None
-    patronymic: Optional[str] = None
-    name: Optional[str] = None
-    surname: Optional[str] = None
-    birthdate: Optional[datetime] = None
-    options: Optional[str] = None
-
-
-class UserUpdate(schemas.BaseUserUpdate):
-    balance: Optional[float] = None
-    patronymic: Optional[str] = None
-    name: Optional[str] = None
-    surname: Optional[str] = None
-    birthdate: Optional[datetime] = None
-    options: Optional[str] = None
