4c7d6d820fdbaf53dc8b9801a72e0b92eb80bb82
Nikita Korzhuk <ssamplerr6@gmail.com>
2024-04-05 02:10:23 +0300

    fix merge conflict

--- src/api/api_v1/user.py
+++ src/api/api_v1/user.py
@@ -7,7 +7,7 @@ router = APIRouter()
 
 router.include_router(
     fastapi_users.get_auth_router(auth_backend),
-    prefix="/auth/jwt",
+    prefix="/auth",
     tags=["auth"],
 )
 
--- src/main.py
+++ src/main.py
@@ -1,24 +1,25 @@
 from api.api import router as api_router
 from exceptions.base import exception_traceback_middleware
 from fastapi import FastAPI
-from fastapi.security import HTTPBearer
+from starlette.middleware.cors import CORSMiddleware
+
+origins = [
+    "http://localhost:8000",
+]
+
 
 app = FastAPI(
     Title="AnalyzeMate-server",
-    swagger_ui_init_oauth={
-        "securityDefinitions": {
-            "Bearer": {
-                "name": "AuthorizationBearer",
-                "in": "header",
-                "type": "apiKey",
-                "description": "HTTP/HTTPS Bearer",
-            }
-        }
-    },
+)
+
+app.add_middleware(
+    CORSMiddleware,  # noqa
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
 )
 
 app.middleware("http")(exception_traceback_middleware)
 
 app.include_router(api_router)
-
-auth_schema = HTTPBearer()
