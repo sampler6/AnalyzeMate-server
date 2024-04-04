cd3dc6cd3fe714c5193fc8282479ca6a23e95897
Nikita Korzhuk <ssamplerr6@gmail.com>
2024-04-04 21:34:12 +0300

    fix user and securities

--- src/auth/models/user.py
+++ src/auth/models/user.py
@@ -3,6 +3,7 @@ from typing import Any
 
 from db.base import Base
 from fastapi_users_db_sqlalchemy import SQLAlchemyBaseUserTable
+from sqlalchemy import DateTime
 from sqlalchemy.dialects.postgresql import JSON
 from sqlalchemy.orm import Mapped, mapped_column
 
@@ -16,5 +17,5 @@ class User(SQLAlchemyBaseUserTable[int], Base):
     patronymic: Mapped[str] = mapped_column(nullable=True)
     name: Mapped[str] = mapped_column(nullable=True)
     surname: Mapped[str] = mapped_column(nullable=True)
-    birthdate: Mapped[datetime] = mapped_column(nullable=False)
+    birthdate: Mapped[datetime] = mapped_column(DateTime(timezone=True), nullable=False)
     config: Mapped[dict[str, Any]] = mapped_column(JSON, nullable=True)
--- src/securities/models/securities.py
+++ src/securities/models/securities.py
@@ -6,9 +6,7 @@ from sqlalchemy.orm import Mapped, mapped_column
 class Securities(Base):
     __tablename__ = "securities"
 
-    uid: Mapped[str] = mapped_column(String, primary_key=True)
-    figi: Mapped[str]
-    ticker: Mapped[str]
+    ticker: Mapped[str] = mapped_column(String, primary_key=True)
     name: Mapped[str]
     price_history: Mapped[int] = mapped_column(Integer, ForeignKey("price_history.id"))
     description: Mapped[str] = mapped_column(nullable=True)
