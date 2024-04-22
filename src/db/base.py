44513a3254fa33182022a5ad1584431c5dad8ac5
sampler6 <ssamplerr6@gmail.com>
2024-04-22 15:22:28 +0300

    Revert "fix"
    This reverts commit ce08986f502fb1548f3140d95a677ea4357b0182.

--- src/db/base.py
+++ src/db/base.py
@@ -1,31 +1,25 @@
-from asyncio import shield
 from typing import AsyncGenerator
 
 import redis.asyncio  # type: ignore
 from config import DB_HOST, DB_NAME, DB_PASS, DB_PORT, DB_USER, REDIS_HOST
-from sqlalchemy import NullPool
 from sqlalchemy.ext.asyncio import AsyncSession, async_sessionmaker, create_async_engine
 from sqlalchemy.orm import DeclarativeBase
 
 DATABASE_URL = f"postgresql+psycopg://{DB_USER}:{DB_PASS}@{DB_HOST}:{DB_PORT}/{DB_NAME}"
 
 
-engine = create_async_engine(DATABASE_URL, poolclass=NullPool)
+engine = create_async_engine(
+    DATABASE_URL, max_overflow=10, pool_pre_ping=5, pool_recycle=-1, pool_size=5, pool_timeout=30
+)
 async_session = async_sessionmaker(engine, expire_on_commit=False)
 
 redis_db = redis.asyncio.from_url(f"redis://{REDIS_HOST}:6379", decode_responses=True)  # type: ignore
 
 
 async def get_async_session() -> AsyncGenerator[AsyncSession, None]:
-    session = async_session()
-    try:
+    async with async_session() as session:
         yield session
         await session.commit()
-    except Exception:
-        await session.rollback()
-        raise
-    finally:
-        await shield(session.close())
 
 
 class Base(DeclarativeBase):
