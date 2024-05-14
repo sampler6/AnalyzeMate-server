b29baa11cf51ad2312498db5fee4521ef3cd0b4f
Nikita Korzhuk <ssamplerr6@gmail.com>
2024-05-14 20:56:18 +0300

    fix

--- src/securities/models/historic_candles.py
+++ src/securities/models/historic_candles.py
@@ -1,18 +1,16 @@
-from datetime import datetime
-
 from db.base import Base
-from sqlalchemy import DateTime, ForeignKey, Integer, func
+from sqlalchemy import ForeignKey
 from sqlalchemy.orm import Mapped, mapped_column
 
 
 class HistoricCandles(Base):
     __tablename__ = "historic_candle"
 
-    id: Mapped[int] = mapped_column(Integer, autoincrement=True, primary_key=True)
     open: Mapped[float]
     close: Mapped[float]
     highest: Mapped[float]
     lowest: Mapped[float]
     volume: Mapped[int]
-    ticker: Mapped[str] = mapped_column(ForeignKey("securities.ticker"))
-    timestamp: Mapped[datetime] = mapped_column(DateTime(timezone=True), server_default=func.now())
+    ticker: Mapped[str] = mapped_column(ForeignKey("securities.ticker"), primary_key=True)
+    timeframe: Mapped[int] = mapped_column(primary_key=True)
+    timestamp: Mapped[float] = mapped_column(primary_key=True)
