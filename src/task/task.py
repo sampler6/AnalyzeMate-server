944193ddf3017065492a2137f14c9a0e522399b9
sampler6 <80578383+sampler6@users.noreply.github.com>
2024-05-15 00:43:00 +0300

    add periodic task (#78)

--- src/task/task.py
+++ src/task/task.py
@@ -7,3 +7,8 @@ app_celery = Celery(
     backend=f"redis://{REDIS_HOST}:6379",
     broker_connection_retry_on_startup=True,
 )
+
+
+app_celery.conf.beat_schedule = {
+    "strategy": {"task": "task.task_strategy.start_strategy", "schedule": 300.0, "args": ()},
+}
--- src/task/task_strategy.py
+++ src/task/task_strategy.py
@@ -1,14 +1,14 @@
-import asyncio
 from datetime import datetime
 from logging import getLogger
 from typing import Any
 
-from services.historic_candle import HistoricCandlesService
-from services.security import SecuritiesService
+from config import LOAD_SECURITIES
+from securities.models import HistoricCandles, Securities
+from sqlalchemy import and_, select
 from strategies.prediction import Prediction
 from tinkoff.invest.grpc.marketdata_pb2 import CANDLE_INTERVAL_30_MIN
 
-from task.base import get_strategies_historic_candles_service, get_strategies_securities_service
+from task.base import sync_session
 from task.task import app_celery
 
 logger = getLogger("api")
@@ -16,34 +16,40 @@ logger = getLogger("api")
 
 @app_celery.task(default_retry_delay=2 * 5, max_retries=2)
 def start_strategy() -> None:
-    security_service: SecuritiesService = asyncio.run(anext(get_strategies_securities_service))  # type: ignore
-    historic_candle_service: HistoricCandlesService = asyncio.run(anext(get_strategies_historic_candles_service))  # type: ignore
+    if not LOAD_SECURITIES:
+        return
+    with sync_session() as session:
+        securities = session.execute(select(Securities)).scalars().all()
 
-    securities = asyncio.run(security_service.get_all_securities())
+        for security in securities:
+            data: dict[str, Any] = {
+                "figi": "BBG004RVFFC0",
+                "ticker": security.ticker,
+                "timeframe": CANDLE_INTERVAL_30_MIN,
+                "history": [["time", "open", "close", "high", "low", "volume"]],
+            }
 
-    for security in securities:
-        data: dict[str, Any] = {
-            "figi": "BBG004RVFFC0",
-            "ticker": security.ticker,
-            "timeframe": CANDLE_INTERVAL_30_MIN,
-            "history": [["time", "open", "close", "high", "low", "volume"]],
-        }
-        candles_db = sorted(
-            asyncio.run(historic_candle_service.get_historic_candles_by_ticker_and_timeframe(security.ticker, 9)),
-            key=lambda x: x.timestamp,
-        )
-
-        for candle in candles_db:
-            data["history"].append(
-                [
-                    datetime.fromtimestamp(candle.timestamp / 1000),
-                    candle.open,
-                    candle.close,
-                    candle.highest,
-                    candle.lowest,
-                    candle.volume,
-                ]
+            candles_db = (
+                session.execute(
+                    select(HistoricCandles)
+                    .where(and_(HistoricCandles.ticker == security.ticker, HistoricCandles.timeframe == 9))
+                    .order_by(HistoricCandles.timestamp)
+                )
+                .scalars()
+                .all()
             )
 
-        predict = Prediction.get_RSI_predict(data)  # type: ignore
-        logger.info(predict)
+            for candle in candles_db:
+                data["history"].append(
+                    [
+                        datetime.fromtimestamp(candle.timestamp / 1000),
+                        candle.open,
+                        candle.close,
+                        candle.highest,
+                        candle.lowest,
+                        candle.volume,
+                    ]
+                )
+
+            predict = Prediction.get_RSI_predict(data)  # type: ignore
+            logger.info(predict)
